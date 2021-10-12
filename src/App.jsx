193a78fb9613039c44a2387ce6ce073d257e081c
Ambera543 <g.eismontiene@gmai.com>
2021-10-12 12:47:04 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import './App.css';
 
-//import Box from './Components/Box';
 import Simple from './Components/Simple';
 
 
@@ -63,41 +61,3 @@ function App() {
 
 export default App;
 
-
-
-// const a = 5;
-// const b = 6;
-
-// const data = [
-//   {
-//     ls: '2px',
-//     boxColor: 'green',
-//     boxNumber: 68
-//   },
-//     {
-//     ls: '2px',
-//     boxColor: 'yellow',
-//     boxNumber: 45
-//   },
-//     {
-//     ls: '2px',
-//     boxColor: 'white',
-//     boxNumber: 90
-//   },
-//     {
-//     ls: '4px',
-//     boxColor: 'pink',
-//     boxNumber: 101
-//   }
-// ];
-
-
-// function App() {
-
-//   return (
-//     <>
-//     {data.map((box, index) => <Box key={index} boxColor={box.boxColor} ls={box.ls} boxNumber={box.boxNumber} />)}
-//     </>
-//   );
-
-// }
