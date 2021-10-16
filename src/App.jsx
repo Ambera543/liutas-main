2f18d315daa37bc6024b8256fe7adb9d791a14b4
Ambera543 <g.eismontiene@gmai.com>
2021-10-16 14:59:28 +0300

    Balloon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,99 @@
-import Balloon from "./Components/Balloon";
+// import Balloon from "./Components/Balloon";
+import React, { useState } from "react";
 
+
+
+const generateColor = () => {
+  const colors = Math.floor(Math.random()*16777215).toString(16)
+  return  '#'+ colors;
+};
 
 function App() {
-
-    return (
-        <div >
-            <Balloon/>           
-        </div>
-    );
-
-}
+    const balloonArr = Array(10)
+  .fill()
+  .map((_, i) => ({ id: i, bum: false, color: generateColor() }));
+    const [count, setCount] = useState(0)
+    const [balloons, setBalloons] = useState(balloonArr)
+    const onPop = (index) => {
+            setBalloons((balloons) => {
+              const b = [...balloons];
+              b[index].bum = true;
+              return b;
+            })
+          };
+          return (
+         <>
+              <div>
+                {balloons.map((b, i) => {
+                  if (!b.bum) {
+                    return (
+                      <div className="balloon-container" key={b.id}>
+                        <div
+                          className="balloon"
+                          style={{ backgroundColor: b.color }}
+                          onClick={()=>setCount(count+1) || onPop(i)}
+                        ></div>
+                      </div>
+                    );
+                  } 
+                  else {
+                    return <div className="balloon-container">{i+1}</div>;
+                  }
+                })}
+              </div> 
+              <div><h1 className="counter-container">You boom {count} balloon(s)</h1></div>
+           </>
+          );
+        }
+    
 
 export default App;
+
+
+// const colors = ["red", "green", "blue", "yellow", "pink", "purple", "brown", "orange", "gold"];
+// const generateColor = () => {
+//   const index = Math.floor(Math.random() * colors.length);
+//   return colors[index];
+// };
+// const counter=()=>
+// ({ const [count, setCount]} = useState(0));
+// return (
+//   <div onClick={()=>setCount(Count+1)}><p>{count}</p></div>);
+
+// const balloonArr = Array(10)
+//   .fill()
+//   .map((_, i) => ({ id: i, bum: false, color: generateColor() }));
+// export default function App() {
+//   const [balloons, setBalloons] = useState(balloonArr);
+//   const onPop = (index) => {
+//     setBalloons((balloons) => {
+//       const b = [...balloons];
+//       b[index].bum = true;
+//       return b;
+//     })
+//   };
+//   return (
+//  <>
+//       <div>
+//         {balloons.map((b, i) => {
+//           if (!b.bum) {
+//             return (
+//               <div className="balloon-container" key={b.id}>
+//                 <div
+//                   className="balloon"
+//                   style={{ backgroundColor: b.color }}
+//                   onClick={() => onPop(i)}
+//                 ></div>
+//               </div>
+//             );
+//           } else {
+//             return <div className="balloon-container">{i+1}</div>;
+//           }
+//         })}
+
+//       </div> 
+//    </>
+//   );
+// }
+
+// export default  App
--- src/Components/Balloon.jsx
+++ src/Components/Balloon.jsx
@@ -1,45 +1,53 @@
 // import { Component } from 'react';
 
-import React, { useState } from "react";
-const colors = ["red", "green", "blue", "yellow", "pink", "purple", "black", "orange"];
-const generateColor = () => {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
-};
-const balloonArr = Array(10)
-  .fill()
-  .map((_, i) => ({ id: i, popped: false, color: generateColor() }));
-export default function App() {
-  const [balloons, setBalloons] = useState(balloonArr);
-  const onPop = (index) => {
-    setBalloons((balloons) => {
-      const b = [...balloons];
-      b[index].popped = true;
-      return b;
-    });
-  };
-  return (
-    <div className="App">
-      <div>
-        {balloons.map((b, i) => {
-          if (!b.popped) {
-            return (
-              <div className="balloon-container" key={b.id}>
-                <div
-                  className="balloon"
-                  style={{ backgroundColor: b.color }}
-                  onClick={() => onPop(i)}
-                ></div>
-              </div>
-            );
-          } else {
-            return <div className="balloon-container">{b.id}</div>;
-          }
-        })}
-      </div>
-    </div>
-  );
-}
+// import React, { useState } from "react";
+// const colors = ["red", "green", "blue", "yellow", "pink", "purple", "brown", "orange", "gold"];
+// const generateColor = () => {
+//   const index = Math.floor(Math.random() * colors.length);
+//   return colors[index];
+// };
+// const counter=()=>
+// ({ const [count, setCount]} = useState(0));
+// return (
+//   <div onClick={()=>setCount(Count+1)}><p>{count}</p></div>);
+
+// const balloonArr = Array(10)
+//   .fill()
+//   .map((_, i) => ({ id: i, bum: false, color: generateColor() }));
+// export default function App() {
+//   const [balloons, setBalloons] = useState(balloonArr);
+//   const onPop = (index) => {
+//     setBalloons((balloons) => {
+//       const b = [...balloons];
+//       b[index].bum = true;
+//       return b;
+//     })
+//   };
+//   return (
+//  <>
+//       <div>
+//         {balloons.map((b, i) => {
+//           if (!b.bum) {
+//             return (
+//               <div className="balloon-container" key={b.id}>
+//                 <div
+//                   className="balloon"
+//                   style={{ backgroundColor: b.color }}
+//                   onClick={() => onPop(i)}
+//                 ></div>
+//               </div>
+//             );
+//           } else {
+//             return <div className="balloon-container">{i+1}</div>;
+//           }
+//         })}
+
+//       </div> 
+//    </>
+//   );
+// }
+
+// export default  App
 // class Balloon extends Component {
 
 //     constructor(props) {
